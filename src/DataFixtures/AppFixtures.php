7e30d62bafdc31f9e4332428cb63c96532874518
Lina Amelal <lina.amelal@telecom-sudparis.eu>
2025-10-10 20:23:16 +0200

    Fixtures: add Member with hashed passwords)

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,25 +4,49 @@ namespace App\DataFixtures;
 
 use App\Entity\CloudBox;
 use App\Entity\CloudPhoto;
+use App\Entity\Member;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    private UserPasswordHasherInterface $hasher;
+
+    public function __construct(UserPasswordHasherInterface $hasher)
+    {
+        $this->hasher = $hasher;
+    }
+
+    /** @return \Generator<array{0:string,1:string,2:string}> */
+    private function membersGenerator(): \Generator
+    {
+        yield ['olivier@localhost', '123456', 'Olivier'];
+        yield ['slash@localhost',   '123456', 'Slash'];
+    }
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
-        $boxA = (new CloudBox())->setDescription('Boîte A - Nuages stratifiés');
-        $boxB = (new CloudBox())->setDescription('Boîte B - Cumulus');
+        // Données démo CloudBox / CloudPhoto (déjà présentes dans ton projet)
+        $boxA = (new CloudBox())->setDescription('Boîte A : Nuages stratifiés');
+        $boxB = (new CloudBox())->setDescription('Boîte B : Cumulus');
         $manager->persist($boxA);
         $manager->persist($boxB);
-        
-        $p1 = (new CloudPhoto())->setDescription('Cumulus au coucher du soleil')->setBox($boxB);
-        $p2 = (new CloudPhoto())->setDescription('Stratus bas sur Paris')->setBox($boxA);
-        
-        $manager->persist($p1);
-        $manager->persist($p2);
+
+        $manager->persist((new CloudPhoto())->setDescription('Cumulus au coucher du soleil')->setBox($boxB));
+        $manager->persist((new CloudPhoto())->setDescription('Stratus bas sur Paris')->setBox($boxA));
+
+        // Membres
+        foreach ($this->membersGenerator() as [$email, $plainPassword, $nom]) {
+            $user = new Member();
+            $user->setEmail($email);
+            $user->setNom($nom); // requis car not null
+
+            $hashed = $this->hasher->hashPassword($user, $plainPassword);
+            $user->setPassword($hashed);
+
+            $manager->persist($user);
+        }
 
         $manager->flush();
     }
